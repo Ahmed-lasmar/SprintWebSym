9e78b61462e4324dbaa45de5a5e481481e07a6ba
Ahmed-lasmar <91993395+Ahmed-lasmar@users.noreply.github.com>
2022-11-10 14:20:18 +0100

    .env (#28)
    * add iheb
    
    * Update .env

---
+++ src/Entity/Participation.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Participation
+ *
+ * @ORM\Table(name="participation")
+ * @ORM\Entity
+ */
+class Participation
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="idEvent", type="integer", nullable=false)
+     */
+    private $idevent;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="idParticipation", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idparticipation;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="idUser", type="integer", nullable=false)
+     */
+    private $iduser;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Pmail", type="string", length=30, nullable=false)
+     */
+    private $pmail;
+
+    public function getIdevent(): ?int
+    {
+        return $this->idevent;
+    }
+
+    public function setIdevent(int $idevent): self
+    {
+        $this->idevent = $idevent;
+
+        return $this;
+    }
+
+    public function getIdparticipation(): ?int
+    {
+        return $this->idparticipation;
+    }
+
+    public function getIduser(): ?int
+    {
+        return $this->iduser;
+    }
+
+    public function setIduser(int $iduser): self
+    {
+        $this->iduser = $iduser;
+
+        return $this;
+    }
+
+    public function getPmail(): ?string
+    {
+        return $this->pmail;
+    }
+
+    public function setPmail(string $pmail): self
+    {
+        $this->pmail = $pmail;
+
+        return $this;
+    }
+
+
+}
