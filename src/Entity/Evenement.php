637a720e94dd9ee23798809c3c93ed9351a117f7
Ahmed-lasmar <91993395+Ahmed-lasmar@users.noreply.github.com>
2022-11-10 11:06:15 +0100

    add iheb

---
+++ src/Entity/Evenement.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Evenement
+ *
+ * @ORM\Table(name="evenement")
+ * @ORM\Entity
+ */
+class Evenement
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="idEvent", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idevent;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="idUser", type="integer", nullable=false)
+     */
+    private $iduser;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=20, nullable=false)
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="dateEvent", type="string", length=20, nullable=false)
+     */
+    private $dateevent;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adresse", type="string", length=20, nullable=false)
+     */
+    private $adresse;
+
+    public function getIdevent(): ?int
+    {
+        return $this->idevent;
+    }
+
+    public function getIduser(): ?int
+    {
+        return $this->iduser;
+    }
+
+    public function setIduser(int $iduser): self
+    {
+        $this->iduser = $iduser;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDateevent(): ?string
+    {
+        return $this->dateevent;
+    }
+
+    public function setDateevent(string $dateevent): self
+    {
+        $this->dateevent = $dateevent;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+
+}
