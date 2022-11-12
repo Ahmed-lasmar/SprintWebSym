3a08b518f61cb00df8ace8306b42ca1e50647405
Ahmed-lasmar <ahmed.lasmar@esprit.tn>
2022-11-12 13:11:59 +0100

    b

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+#[Route('/login')]
+class LoginController extends AbstractController
+{
+    #[Route('/', name: 'app_login')]
+    public function index(): Response
+    {
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'LoginController',
+        ]);
+    }
+}
