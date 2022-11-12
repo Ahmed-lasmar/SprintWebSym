42fe51a3ceb016eb34c49f2c04400467c5775510
Ahmed-lasmar <ahmed.lasmar@esprit.tn>
2022-11-12 15:04:51 +0100

    DB

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\LoginType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 #[Route('/login')]
 class LoginController extends AbstractController
 {
     #[Route('/', name: 'app_login')]
-    public function index(): Response
+    public function index(Request $request, ManagerRegistry $doctrine): Response
     {
+        $user=new User();
+        $form=$this->createForm(LoginType::class,$user );
+        $form->handleRequest($request);
+        if ($form->isSubmitted()){
+            $em=$doctrine->getManager();
+            $em->persist($user);
+            $em->flush();
+        }
         return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
+            'form' => $form->createView(),
         ]);
     }
 }
