d64c0f0f87633393c62d5b2278f2266e94b87384
Ahmed-lasmar <ahmed.lasmar@esprit.tn>
2022-11-13 19:18:22 +0100

    user

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,25 +14,25 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('email')
             ->add('nom')
             ->add('prenom')
-            ->add('email')
             ->add('cin')
             ->add('urlPhoto')
             ->add('dateDeNaissance')
             ->add('numTel')
-            ->add('dateEmbauche')
-            ->add('grade')
-            ->add('equipe')
-            ->add('role')
-            ->add('mdp')
+            ->add('mdp',RepeatedType::class, [
+                'type'=>PasswordType::class,
+                'first_options'=>['label'=>'Password'],
+                'second_options'=>['label'=>'Confirm Password']
+            ])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => User::class,
+            // Configure your form options here
         ]);
     }
 }
