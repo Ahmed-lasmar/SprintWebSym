7792699e253d1191815ecb7f2952d06cea6dbf0f
Ahmed-lasmar <91993395+Ahmed-lasmar@users.noreply.github.com>
2022-11-14 21:53:32 +0100

    Update bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -9,6 +9,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
